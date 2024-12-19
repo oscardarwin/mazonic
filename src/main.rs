2c33f523f3db9abbd082ea1ddb8de97ff64bad58
Hallayus <hallayus@gmail.com>
2024-12-19 11:15:06 +0100

    removing unnused code. Cleaning up readme

--- src/main.rs
+++ src/main.rs
@@ -32,20 +32,3 @@ fn load_maze(mut commands: Commands) {
     let maze = CubeMaze::build(3, 2.0, 0.2);
     commands.insert_resource(maze);
 }
-
-fn setup(
-    commands: Commands,
-    meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    cube_maze: Res<CubeMaze>,
-) {
-    let white = Color::srgb_u8(247, 247, 0);
-    let beige = Color::srgb_u8(242, 231, 213);
-    let green = Color::srgb_u8(109, 152, 134);
-    let charcoal = Color::srgb_u8(57, 62, 70);
-
-    let white_material = materials.add(StandardMaterial::from_color(white));
-    let beige_material = materials.add(StandardMaterial::from_color(beige));
-    let green_material = materials.add(StandardMaterial::from_color(green));
-    let charcoal_material = materials.add(StandardMaterial::from_color(charcoal));
-}
