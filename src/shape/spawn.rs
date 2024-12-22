46aa2bc29664be881e0fc0ad29a5ef4edd8cb539
Hallayus <hallayus@gmail.com>
2024-12-22 19:03:54 +0100

    adding face angle and distance between nodes to line builder

--- src/shape/spawn.rs
+++ src/shape/spawn.rs
@@ -90,16 +90,14 @@ pub fn spawn_shape_meshes<P: PlatonicSolid>(
     }
 
     let face_angle = FRAC_PI_2;
-    let edge_mesh_builder = EdgeMeshBuilder::new();
-    let distance_between_nodes = level.platonic_solid.distance_between_nodes();
+    let edge_mesh_builder =
+        EdgeMeshBuilder::new(level.platonic_solid.distance_between_nodes(), face_angle);
 
-    let face_connection_mesh = meshes.add(edge_mesh_builder.line(distance_between_nodes));
-    let face_arrow_mesh = meshes.add(edge_mesh_builder.dashed_arrow(distance_between_nodes));
+    let face_connection_mesh = meshes.add(edge_mesh_builder.line());
+    let face_arrow_mesh = meshes.add(edge_mesh_builder.dashed_arrow());
 
-    let edge_mesh =
-        meshes.add(edge_mesh_builder.edge_line(distance_between_nodes / 2.0, face_angle));
-    let edge_arrow_mesh =
-        meshes.add(edge_mesh_builder.dashed_arrow_edge(distance_between_nodes / 2.0, face_angle));
+    let edge_mesh = meshes.add(edge_mesh_builder.edge_line());
+    let edge_arrow_mesh = meshes.add(edge_mesh_builder.dashed_arrow_edge());
 
     for (source_node, target_node, _) in level.maze.graph.all_edges() {
         let bidirectional = level.maze.graph.contains_edge(target_node, source_node);
@@ -175,11 +173,13 @@ struct EdgeMeshBuilder {
     dash_length: f32,
     min_spacing: f32,
     arrow_head_width: f32,
+    face_angle: f32,
+    distance_between_nodes: f32,
     empty_mesh: Mesh,
 }
 
 impl EdgeMeshBuilder {
-    pub fn new() -> Self {
+    pub fn new(distance_between_nodes: f32, face_angle: f32) -> Self {
         let empty_mesh = Mesh::new(
             PrimitiveTopology::TriangleList,
             RenderAssetUsages::default(),
@@ -194,11 +194,17 @@ impl EdgeMeshBuilder {
             dash_length: 0.09,
             min_spacing: 0.07,
             arrow_head_width: 0.12,
+            face_angle,
+            distance_between_nodes,
             empty_mesh,
         }
     }
 
-    pub fn line(&self, length: f32) -> Mesh {
+    pub fn line(&self) -> Mesh {
+        self.make_line(self.distance_between_nodes)
+    }
+
+    fn make_line(&self, length: f32) -> Mesh {
         Rectangle::new(self.dash_width, length)
             .mesh()
             .build()
@@ -206,7 +212,7 @@ impl EdgeMeshBuilder {
             .translated_by(Vec3::Z * length / 2.0)
     }
 
-    fn dashed_line(&self, length: f32) -> Mesh {
+    fn make_dashed_line(&self, length: f32) -> Mesh {
         let total_min_segment_length = self.dash_length + self.min_spacing;
 
         let num_dashes = (length / total_min_segment_length).floor();
@@ -219,14 +225,18 @@ impl EdgeMeshBuilder {
         for dash_index in 0..num_dashes as u8 {
             let z_coord = dash_and_space_half_length + dash_index as f32 * dash_and_space_length;
             let position = z_coord * line_direction;
-            let dash_mesh = self.dash(position);
+            let dash_mesh = self.make_dash(position);
 
             mesh.merge(&dash_mesh);
         }
         mesh
     }
 
-    pub fn dashed_arrow(&self, length: f32) -> Mesh {
+    pub fn dashed_arrow(&self) -> Mesh {
+        self.make_dashed_arrow(self.distance_between_nodes)
+    }
+
+    fn make_dashed_arrow(&self, length: f32) -> Mesh {
         let total_min_segment_length = self.dash_length + self.min_spacing;
 
         let num_dashes = (length / total_min_segment_length).floor();
@@ -238,7 +248,7 @@ impl EdgeMeshBuilder {
         for dash_index in 0..(num_dashes as u8 - 1) {
             let z_coord = dash_and_space_half_length + dash_index as f32 * dash_and_space_length;
             let position = z_coord * Vec3::Z;
-            let dash_mesh = self.dash(position);
+            let dash_mesh = self.make_dash(position);
 
             mesh.merge(&dash_mesh);
         }
@@ -246,34 +256,38 @@ impl EdgeMeshBuilder {
         let arrow_z_coord =
             dash_and_space_half_length + (num_dashes as f32 - 1.0) * dash_and_space_length;
         let position = Vec3::Z * arrow_z_coord;
-        mesh.merge(&self.arrow(position));
+        mesh.merge(&self.make_arrow(position));
 
         mesh
     }
 
-    pub fn dashed_arrow_edge(&self, half_length: f32, half_plane_angle: f32) -> Mesh {
-        let mut first_dashed_line = self.dashed_arrow(half_length);
+    pub fn dashed_arrow_edge(&self) -> Mesh {
+        let half_length = self.distance_between_nodes / 2.0;
+
+        let mut first_dashed_line = self.make_dashed_arrow(half_length);
         let second_dashed_line = self
-            .dashed_line(half_length)
+            .make_dashed_line(half_length)
             .rotated_by(Quat::from_rotation_z(PI))
-            .rotated_by(Quat::from_rotation_x(PI - half_plane_angle));
+            .rotated_by(Quat::from_rotation_x(PI - self.face_angle));
 
         first_dashed_line.merge(&second_dashed_line);
         first_dashed_line
     }
 
-    pub fn edge_line(&self, half_length: f32, half_plane_angle: f32) -> Mesh {
-        let mut first_line = self.line(half_length);
+    pub fn edge_line(&self) -> Mesh {
+        let half_length = self.distance_between_nodes / 2.0;
+
+        let mut first_line = self.make_line(half_length);
         let second_line = self
-            .line(half_length)
+            .make_line(half_length)
             .rotated_by(Quat::from_rotation_z(PI))
-            .rotated_by(Quat::from_rotation_x(PI - half_plane_angle));
+            .rotated_by(Quat::from_rotation_x(PI - self.face_angle));
 
         first_line.merge(&second_line);
         first_line
     }
 
-    fn dash(&self, position: Vec3) -> Mesh {
+    fn make_dash(&self, position: Vec3) -> Mesh {
         Rectangle::new(self.dash_width, self.dash_length)
             .mesh()
             .build()
@@ -281,7 +295,7 @@ impl EdgeMeshBuilder {
             .translated_by(position)
     }
 
-    fn arrow(&self, position: Vec3) -> Mesh {
+    fn make_arrow(&self, position: Vec3) -> Mesh {
         let arrow_side_vertex = Vec3::new(self.arrow_head_width / 2.0, 0.0, 0.0);
         let arrow_tip_vertex = Vec3::new(0.0, 0.0, self.dash_length / 2.0);
 
