bf276ca886797ced8cb4ed8d1276b539ef89b079
Hallayus <hallayus@gmail.com>
2024-12-24 06:42:17 +0100

    adding player setup logic to loader.rs

--- src/player.rs
+++ src/player.rs
@@ -26,49 +26,8 @@ pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, move_player::<Tetrahedron>)
-            .add_systems(Startup, setup_player::<Tetrahedron>.after(load_maze));
+        app.add_systems(Update, move_player::<Tetrahedron>);
     }
-}
-
-pub fn setup_player<P: PlatonicSolid>(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-    settings: Res<GameSettings>,
-    level: Res<Level<P>>,
-) {
-    let white = Color::srgb_u8(247, 247, 0);
-
-    let white_material = materials.add(StandardMaterial::from_color(white));
-
-    let initial_node = level.maze.solution.first().unwrap().clone();
-    let player_transform =
-        compute_initial_player_transform::<P>(initial_node, settings.player_elevation);
-    let player_shape = Sphere::new(0.1);
-    let player_mesh = meshes.add(player_shape);
-
-    commands
-        .spawn(PbrBundle {
-            mesh: Mesh3d(player_mesh),
-            material: MeshMaterial3d(white_material.clone()),
-            transform: player_transform,
-            ..default()
-        })
-        .insert(Player)
-        .insert(PlayerMazeState::<P>::Node(initial_node))
-        .insert(Collider::ball(player_shape.radius));
-}
-
-fn compute_initial_player_transform<P: PlatonicSolid>(
-    start_node: P::Room,
-    player_elevation: f32,
-) -> Transform {
-    let face_normal = start_node.face().normal();
-
-    Transform::IDENTITY
-        .looking_at(face_normal.any_orthogonal_vector(), face_normal)
-        .with_translation(start_node.position() + player_elevation * face_normal)
 }
 
 fn move_player<P: PlatonicSolid>(
