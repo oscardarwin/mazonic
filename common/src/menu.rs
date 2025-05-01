0d6723ed3551ab55f0f15a14a82c3feaac107f88
Hallayus <hallayus@gmail.com>
2025-05-01 21:37:01 +0200

    added functionality to go directly to first level instead of selector on a fresh save

--- common/src/menu.rs
+++ common/src/menu.rs
@@ -1,8 +1,14 @@
 use bevy::prelude::*;
 
-use crate::game_state::GameState;
+use crate::{game_save::WorkingLevelIndex, game_state::GameState};
 
-pub fn setup(mut next_game_state: ResMut<NextState<GameState>>) {
-    println!("loading menu");
-    next_game_state.set(GameState::Selector)
+pub fn setup(mut next_game_state: ResMut<NextState<GameState>>, working_level_index_query: Query<&WorkingLevelIndex>) {
+    let WorkingLevelIndex(index) = working_level_index_query.single();
+
+    if *index > 0 {
+        next_game_state.set(GameState::Selector)
+    } else {
+        next_game_state.set(GameState::Playing)
+    }
+
 }
